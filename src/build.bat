2a8e2d3c7326ea2838245f33725ce039c454fe51
lightspear@163.com <lightspear@163.com>
2022-05-25 09:23:24 +0800

    修订readme

--- src/build.bat
+++
@@ -1,3 +0,0 @@
-call bash build_win.sh
-call bash build_crossTolinux64.sh
-call bash build_crossTolinuxArm64.sh
--- src/build_win.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-go build -ldflags "-s -w" -o src.exe ./main.go
-# -o ../Client/hycrecv1.exe
-mkdir -p ../Client
-rm -rvf ../Client/mangos-filetransfer.exe
-upx -o ../Client/mangos-filetransfer.exe src.exe
-rm -rvf src.exe
